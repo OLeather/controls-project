6ea819d4b982a00aacefe3e46052abafaab60abd
oleather <owenleather03@gmail.com>
2022-08-20 16:10:22 -0700

    cleanup pendulum cart example

--- controls.py
+++ controls.py
@@ -1,3 +1,7 @@
+import numpy as np
+import scipy.linalg
+
+
 # Returns the controllability matrix of the system
 # C = [B AB A^2B ... A^(n-1)B]
 def ctrb(A, B):
@@ -7,3 +11,15 @@ def ctrb(A, B):
 # Returns the control K matrix given a set of eigenvalues and system matrices
 def place(A, B, eigs):
     return []
+
+
+def lqr_c(A, B, Q, R):
+    P = scipy.linalg.solve_continuous_are(A, B, Q, R)
+    K = np.linalg.inv(R) * (B.T * P)
+    return K
+
+
+def lqr_d(A, B, Q, R):
+    P = scipy.linalg.solve_discrete_are(A, B, Q, R)
+    K = np.linalg.inv(R + B.T * P * B) * (B.T * P * A)
+    return K
--- pendulum_cart.py
+++ pendulum_cart.py
@@ -4,25 +4,15 @@ from math import sin, pi, cos
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
 from numpy.linalg import linalg
 from scipy import signal
+
+from controls import lqr_c
 
 matplotlib.use("TkAgg")
 
 from sim import sim_pendulum_cart
-
-
-def plot_pendulum_cart(x, theta, line, line1, line2):
-    theta -= pi / 2
-    line.set_xdata([x, x + 3*cos(theta)])
-    line.set_ydata([0, 3*sin(theta)])
-    line1.set_xdata([x-.5, x+.5])
-    line1.set_ydata([0, 0])
-    line2.set_xdata([x, x+3*cos(theta)])
-    line2.set_ydata([0.05, 3*sin(theta)])
-    # ax.plot([x, x + cos(theta)], [0, sin(theta)], label="x")
-    plt.pause(0.01)
-
 
 # Example: Inverted Pendulum on Cart http://databookuw.com/databook.pdf#page=372
 if __name__ == "__main__":
@@ -38,7 +28,7 @@ if __name__ == "__main__":
 
     x = np.matrix([[-2],  # x
                    [0],  # x_dot
-                   [pi],  # theta
+                   [pi+.1],  # theta
                    [0]])  # theta_dot
 
     # Linearized system matrices
@@ -54,7 +44,11 @@ if __name__ == "__main__":
 
     poles = [-2, -2.1, -2.2, -2.3]
 
-    K = signal.place_poles(A, B, poles).gain_matrix
+    # K = signal.place_poles(A, B, poles).gain_matrix
+
+    Q = np.diag([50, 50, 10, 100])
+    R = np.matrix([[.001]])
+    K = lqr_c(A, B, Q, R)
 
     print("K:", K)
 
@@ -70,19 +64,9 @@ if __name__ == "__main__":
     us = []
     ts = []
 
-    fig, ax = plt.subplots()
-
-    line, = ax.plot([], [])
-    line1, = ax.plot([], [], color='black')
-    line2, = ax.plot([], [], 'o', color='black')
-    plt.xlim([-5, 5])
-    plt.ylim([-5, 5])
-    plt.grid()
-    plot_pendulum_cart(float(x[0]), float(x[2]), line, line1, line2)
-    plt.pause(2)
+    # plt.pause(2)
     for t in np.arange(0, 10, dt):
         u = -K * (x - x_ref)
-        # print("x:", x, "u:", u)
         # u = np.matrix([[0]])
         x, x_dot, theta, theta_dot = sim_pendulum_cart(m, M, L, d, g, x, u, dt)
         x = np.matrix([[x],
@@ -93,17 +77,29 @@ if __name__ == "__main__":
         thetas.append(float(x[2]))
         us.append(float(u))
         ts.append(t)
-        plot_pendulum_cart(float(x[0]), float(x[2]), line, line1, line2)
-        # plt.xlim([-5, 5])
-        # plt.ylim([-1, 2])
-        # plt.grid()
+
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    line, = ax.plot([], [])
+    line1, = ax.plot([], [], color='black')
+    line2, = ax.plot([], [], 'o', color='black')
+    plt.xlim([-5, 5])
+    plt.ylim([-3, 3])
+    plt.grid()
+
+
+    def animate(i):
+        x = xs[i]
+        theta = thetas[i] - pi / 2
+        line.set_xdata([x, x + 2 * cos(theta)])
+        line.set_ydata([0, 2 * sin(theta)])
+        line1.set_xdata([x - .5, x + .5])
+        line1.set_ydata([0, 0])
+        line2.set_xdata([x, x + 2 * cos(theta)])
+        line2.set_ydata([0.05, 2 * sin(theta)])
+        return line, line1, line2
+
+
+    anim = FuncAnimation(fig, animate, frames=len(xs), interval=dt*1000, blit=True)
 
     plt.show()
-
-    # fig, ax = plt.subplots()
-    # line1, = ax.plot(ts, xs, label="x")
-    # line2, = ax.plot(ts, thetas, label="theta")
-    # line3, = ax.plot(ts, us, label="u")
-    # ax.legend(handles=[line1, line2, line3])
-    # ax.grid(True)
-    # plt.show()
